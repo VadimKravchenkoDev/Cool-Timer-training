bc866e086dbad54086d3ef33b9979ba6d68b80c5
Arhizmei <arhizmei@gmail.com>
2025-05-07 21:08:27 +0300

    add selected options

--- app/src/main/java/com/kravchenkovadim/cooltimerkotlin/MainActivity.java
+++ app/src/main/java/com/kravchenkovadim/cooltimerkotlin/MainActivity.java
@@ -1,13 +1,17 @@
 package com.kravchenkovadim.cooltimerkotlin;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
@@ -104,5 +108,26 @@ public class MainActivity extends AppCompatActivity {
         textView.setText("00:59");
         isTimerOn = false;
         countDownTimer.cancel();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.settings){
+            Intent intent = new Intent(this, Settings.class);
+            startActivity(intent);
+            return true;
+        } else if(id == R.id.about){
+            Intent intent = new Intent(this, About.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
