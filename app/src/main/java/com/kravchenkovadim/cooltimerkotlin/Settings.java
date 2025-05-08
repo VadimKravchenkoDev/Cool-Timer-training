b5c9d5de9ec1ab9c3b489ede549b62c29c555824
Vadim Kravchenko <kravchenko.vadim.dev@gmail.com>
2025-05-08 21:23:33 +0300

    add enabled sound

--- app/src/main/java/com/kravchenkovadim/cooltimerkotlin/Settings.java
+++ app/src/main/java/com/kravchenkovadim/cooltimerkotlin/Settings.java
@@ -20,13 +20,6 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
